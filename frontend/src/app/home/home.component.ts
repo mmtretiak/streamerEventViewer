352006e6c04e9af172f910d513349212c05d0781
Maksym Tretiak <mmtretiak@gmail.com>
2020-08-10 20:18:25 +0300

    Fix login works message

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -35,15 +35,21 @@ export class HomeComponent implements OnInit {
         this.streamers.map(x => {
           x.streamURL = 'https://player.twitch.tv/?channel=' + x.name + '&parent=localhost&muted=true';
           x.chatURL = 'https://www.twitch.tv/embed/' + x.name + '/chat?parent=localhost';
-          const view = viewsRes.find(view => view.streamer_id == x.id);
-          if (view !== undefined) {
-            x.totalViews = view.count;
-          } else {
+          if (viewsRes === null) {
             x.totalViews = 0;
+          } else {
+            const view = viewsRes.find(view => view.streamer_id == x.id);
+            if (view !== undefined && view !== null) {
+              x.totalViews = view.count;
+            } else {
+              x.totalViews = 0;
+            }
           }
           return x;
         })
         this.activeStreamer = this.streamers[0];
+
+        console.log(this.streamers);
       })
     });
   }
@@ -63,8 +69,10 @@ export class HomeComponent implements OnInit {
   }
 
   createClip() {
-    this.clipService.addClip(this.activeStreamer.id).subscribe(() => {
+    this.clipService.addClip(this.activeStreamer.id).subscribe((res) => {
       this.notificationService.success("Clip added!");
+      window.open(res.edit_url)
+      console.log(res.edit_url)
     }, error => this.notificationService.warn(error));
   }
 }
